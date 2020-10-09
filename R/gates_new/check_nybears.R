96df79ff1420b06577e1f2055963f7b7ecf11b1f
Gates Dupont <gld44@cornell.edu>
2020-10-09 17:32:48 -0400

    Used amt package for cleaner analysis.

--- R/gates_new/check_nybears.R
+++ R/gates_new/check_nybears.R
@@ -1,12 +1,12 @@
+library(raster)
+library(ggplot2)
+library(viridis)
+library(patchwork)
 library(oSCR)
 library(dplyr)
 library(tidyr)
-library(ggplot2)
 library(lubridate)
-library(raster)
-library(viridis)
-library(moveHMM)
-select = dplyr::select
+library(amt)
 
 # Get data
 data("nybears")
@@ -20,8 +20,6 @@ ggplot(data = df0, aes(x = Lat, y = Long)) +
   facet_wrap(~animalid, scales = "free") +
   theme_minimal() +
   theme(aspect.ratio = 1)
-
-
 
 "BASIC EXPLORATORY"
 
@@ -65,202 +63,74 @@ apply(diffs.tab, 1, function(x) x[1]/(sum(x[1:4])))
 # Do this row-wise for each cell
 prop.table(table(diffs.summary$id, diffs.summary$diffs.summary), 1)
 
-# Add elevation to ss
-elev <- data.frame(
-  x = nybears$ss$X_UTM, 
-  y = nybears$ss$Y_UTM, 
-  elev = nybears$elevation)
 
-# Plot tracks on raster just to get a sense for resolution
-par(mfrow=c(1,3), oma=c(0,0,0,0),mar=c(1,1,1,1))
-ids <- unique(df0$animalid)
-for(i in 1:3){
-  tmp_df <- df0 %>%
-    filter(animalid == ids[i]) %>%
-    select(x = X_UTM, y = Y_UTM)
+"SPLIT TRACKS"
+tracks_list <- list()
+sl_list <- list()
+
+ids <- df %>% pull(id) %>% unique %>% as.character
+for(i in 1:length(ids)){
   
-  plot(y~x, data = tmp_df, asp = 1)
-  plot(rasterFromXYZ(elev), viridis(1000), add = T, legend = F)
-  lines(y~x, data = tmp_df)
+  tmp_df <- df %>% 
+    filter(id == ids[i]) %>%
+    arrange(time) %>%
+    mk_track(., .x = X_UTM, .y = Y_UTM, .t = time) %>%
+    track_resample(., rate = minutes(60), tolerance = minutes(7)) %>%
+    filter_min_n_burst(min_n = 2) %>%
+    mutate(id = ids[i])
+  
+  tmp_sl <- tmp_df %>%
+    select(-id) %>%
+    steps_by_burst() %>%
+    pull(sl_)
+  
+  tracks_list[[i]] <- tmp_df
+  sl_list[[i]] <- tmp_sl
+  
 }
 
-dev.off()
+tracks_df <- do.call(rbind, tracks_list)
+sl_df <- data.frame(
+  id = c(
+    rep(ids[1], length(sl_list[[1]])),
+    rep(ids[2], length(sl_list[[2]])),
+    rep(ids[3], length(sl_list[[3]]))),
+  sl = unlist(sl_list)
+)
 
-
-"STEP LENGTH"
-
-# Calculate step ratings saving all data
-sldf <- df %>%
-  group_by(id) %>%
-  mutate(diffs = c(as.numeric(diff(time), units = "mins"), NA)) %>%
-  mutate(Index_ind = 1:n()) %>%
-  ungroup() %>%
-  mutate(Index_total = 1:nrow(.)) %>%
-  mutate(diffs.summary0 = if_else(diffs < 70 & diffs > 50, 0, NULL)) %>%
-  mutate(diffs.summary1 = if_else(diffs < 130 & diffs > 110, 1, NULL)) %>%
-  mutate(diffs.summary2 = if_else(diffs < 190 & diffs > 170, 2, NULL)) %>%
-  mutate(diffs.summary3 = if_else(diffs > 190, 3, NULL)) %>%
-  rowwise() %>%
-  mutate(diffs.summary = sum(
-    diffs.summary0, diffs.summary1, diffs.summary2, diffs.summary3, na.rm = T)) %>%
-  select(X_UTM, Y_UTM, id, fixnum, lat, long, time, diffs, diffs.summary) %>%
-  as.data.frame()
-
-# Plot tracks with step ratings
-sldf %>%
-  ggplot(data = ., aes(x = long, y = lat, color = as.factor(diffs.summary))) +
+p1 <- ggplot(data = tracks_df, aes(x = x_, y = y_, group = burst_, color = burst_)) +
   geom_path() +
+  scale_color_viridis("Track ID") +
   facet_wrap(~id, scales = "free") +
-  labs(color = "Missed fixes") +
+  labs(x = "X", y = "Y") +
+  theme_minimal() +
+  theme(aspect.ratio = 1, legend.position = "none", axis.text = element_blank())
+
+p2 <- ggplot(data = sl_df, aes(sl)) +
+  geom_histogram() +
+  facet_wrap(~id, scales = "free_y") +
+  xlab("Step length") +
   theme_minimal() +
   theme(aspect.ratio = 1)
 
-# Parse contiguous tracks
-sldf_trackid <- sldf %>%
-  arrange(id, time) %>%
-  group_by(id) %>%
-  mutate(track_id = if_else(diffs.summary == 0, 0, 1))
-
-id <- 1
-j = 0
-for(i in 1:nrow(sldf_trackid)){
-  
-  j = j + 1
-  
-  if(as.data.frame(sldf_trackid)[i,"track_id"] == 0){
-    
-    sldf_trackid[i,"track_id"] <- id
-    
-  }else{
-    
-    # Break the track, start new
-    sldf_trackid[i,"track_id"] <- -1
-    id <- id + 1
-    
-  }
-  
-} # NOTE: THIS LOSES A STEP ON THE BACK END OF EACH TRACK ID
-
-bears <- as.character(unique(df$id))
-split_tracks <- list()
-
-# -
-for(b in 1:length(bears)){
-  
-  tmp_df_b <- sldf_trackid %>%
-    as.data.frame() %>%
-    filter(id == bears[b]) %>%
-    arrange(time)
-  
-  # - -
-  for(i in 2:(nrow(tmp_df_b))){
-    
-    # - - -
-    tmp_df <- as.data.frame(tmp_df_b)
-    
-    # - - - If previous fix is part of a track and next fix is is -1, then...
-    if(tmp_df[i-1,"track_id"] != -1 & tmp_df[i,"track_id"] == -1){
-      
-      # - - - - Add that final piece
-      tmp_df_b[i,"track_id"] <- tmp_df[i-1,"track_id"]
-    }
-    
-  }
-  
-  # - -
-  split_tracks[[b]] <- tmp_df_b
-  
-}
-
-split_tracks_df <- do.call(rbind, split_tracks)
-
-sldf_trackid <- split_tracks_df %>%
-  filter(track_id != -1) %>%
-  as.data.frame() %>%
-  arrange(track_id, time)
-
-# Plot contiguous tracks
-sldf_trackid %>%
-  ggplot(data = ., aes(x = long, y = lat, group = track_id, color = track_id)) +
-  scale_color_viridis() +
-  geom_path(lwd=0.5) +
+p3 <- ggplot(data = sl_df, aes(sl)) +
+  geom_histogram() +
+  facet_wrap(~id, scales = "free_y") +
+  xlab("Step length") +
   theme_minimal() +
-  facet_wrap(~id, scales = "free") +
+  xlim(0,50) +
   theme(aspect.ratio = 1)
 
-# Calc step length function
-dist <- function(df){
-  result <- prepData(
-    df %>% select(ID = id, long,lat),
-    type="LL",coordNames=c("long","lat"))
-  
-  return(result$step)
-}
-
-# Calculate step length across tracks
-trx <- unique(sldf_trackid$track_id)
-trx_upsilon <- list()
-for(i in 1:length(trx)){
-  
-  tmp_df <- sldf_trackid %>%
-    filter(track_id == trx[i]) %>%
-    select(ID = id, long, lat) %>%
-    as.data.frame()
-  
-  result <- prepData(tmp_df, type="LL", coordNames = c("long","lat"))
-  
-  trx_upsilon[[i]] <- result$step
-}
-
-upsilon <- unlist(trx_upsilon)
-
-
-
-data.frame()
-
-prepData(data.frame(x = 1:3, y = c(1,2,2)), coordNames = c("x","y"))
+p1 / p2 / p3
 
 
 
 
-dist <- function(df){
-  result <- prepData(
-    df %>% select(ID = id, X_UTM,Y_UTM),
-    type="UTM",coordNames=c("X_UTM","Y_UTM"))
-  
-  return(result$step)
-}
-
-x <- dist(sldf_trackid)
-
-upsilon <- round(mean(x[x<60], na.rm=T))
-
-r_df <- cbind(nybears$ss, z= nybears$elevation) %>% as.data.frame()
-r <- rasterFromXYZ(r_df)
-
-ggplot() +
-  geom_tile(data = r_df, aes(x=X_UTM, y=Y_UTM, fill = z)) +
-  geom_path(data = sldf_trackid, 
-            aes(
-              x = X_UTM, y = Y_UTM, 
-              group = track_id, color = as.factor(track_id))) +
-  coord_equal() +
-  theme(legend.position = "none")
 
 
-#----Lat/Long----
 
-dist <- function(df){
-  result <- prepData(
-    df %>% select(ID = id, long,lat),
-    type="LL",coordNames=c("long","lat"))
-  
-  return(result$step)
-}
 
-x <- dist(sldf_trackid)
 
-hist(x[x<0.05], breaks = 40)
 
-mean(na.rm = T, x[x<0.05])
+
 
