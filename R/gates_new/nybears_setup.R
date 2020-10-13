2ed47514f5dfdeeb76f96aa10758e54ac9fc1bc0
Gates Dupont <gld44@cornell.edu>
2020-10-13 17:33:58 -0400

    First draft of new scenario, tracks for all N inds.

--- R/gates_new/nybears_setup.R
+++ R/gates_new/nybears_setup.R
@@ -5,8 +5,18 @@ library(raster)
 library(ggplot2)
 library(ggforce)
 library(patchwork)
+library(doParallel)
+library(gdistance)
+
+extract <- raster::extract
 
 #----Simulation settings----
+
+# Manual settings
+nfix = 5
+
+# Cost
+alpha2 <- 2
 
 # Movement model
 psi <- 0.3
@@ -23,8 +33,7 @@ autocorr <- 7
 
 # Derived 
 hr95 <- sqrt(5.99) * sigma
-#hr95_lim <- hr95*1  #1.5
-hr95_lim <- 3000
+hr95_lim <- 3000 # this is 3 sigma
 
 
 #----Landscape----
@@ -32,26 +41,32 @@ hr95_lim <- 3000
 landscape0 <- nlm_gaussianfield(
   ncol = ncol, nrow = nrow, 
   resolution = rr, autocorr_range = autocorr)
-landscape <- landscape0^2 # I STILL DON'T LIKE THIS! RESTRICTS LOW COST
-landscape_df <- as.data.frame(landscape, xy=T)
+landscape_r <- landscape0^2 # I STILL DON'T LIKE THIS! RESTRICTS LOW COST
+landscape <- as.data.frame(landscape_r, xy=T)
 
-scr_ss <- landscape_df %>%
+ss <- landscape %>%
   filter(x >= (min(x)+hr95_lim) & x < (max(x)-hr95_lim)) %>%
   filter(y >= (min(y)+hr95_lim) & y < (max(y)-hr95_lim))
 
-ss <- coordinates(scr_ss)
+
+#----Traps----
+
+# trap_array <- ss %>%
+#   filter(x >= (min(x)+hr95_lim) & x < (max(x)-hr95_lim)) %>%
+#   filter(y >= (min(y)+hr95_lim) & y < (max(y)-hr95_lim))
 
 
 #----Activity centers----
   
-acs <- ss[sample(1:nrow(ss), size = N, replace = T),]
+acs <- ss[sample(1:nrow(ss), size = N, replace = T),c("x","y")]
+rownames(acs) <- NULL
 acs_df <- as.data.frame(acs)
 
 
 #----Plotting----
 
 p1 <- ggplot() +
-  geom_tile(data = landscape_df, aes(x=x, y=y, fill=layer)) +
+  geom_tile(data = landscape, aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis_c("Conductance") +
   geom_circle(data = acs_df, aes(x0=x, y0=y, r = hr95), 
               lwd = 0.2, color = alpha("white", 0.65)) +
@@ -68,10 +83,159 @@ text1 <- paste0(
 )
 
 p2 <- ggplot() +
-  annotate("text", x = 4, y = 25, size=8, label = text1) + 
+  annotate("text", x = 0, y = 0, size=8, 
+           label = text1) + 
   theme_void()
 
 p1+p2
+
+
+"COST SURFACE"
+
+#----Cost surface----
+
+# Cost surface
+logcost <- alpha2*landscape_r
+cost <- exp(logcost)
+
+# Create tansition surface
+tr1 <- transition(
+  cost,
+  transitionFunction=function(x) 1/mean(x),
+  directions=16)
+
+# Corrected transition surface
+tr1Corr <-geoCorrection(
+  tr1,
+  multpl=FALSE,
+  scl=FALSE)
+
+
+"SIMULATE MOVEMENT"
+
+# Parallel setup
+ncores = detectCores()-1 # Number of available cores -1 to leave for computer
+cl = makeCluster(ncores) # Make the cluster with that many cores
+registerDoParallel(cl)  
+#clusterExport(cl, varlist = c("e2dist"), envir = environment()) # Export required function to the cores
+
+# Data-collection list
+tracks = list()
+
+# Cluster!
+t0 <- Sys.time()
+tracks <- foreach(j=1:N, .packages = c(.packages())) %dopar% {
+  
+  # Generating movement based on pr(move)
+  moved <- rbinom(nfix, 1, psi) # This should be different for each individual
+  
+  # Get starting position for the individual
+  sbar <- acs[j,]
+  sbar_x <- as.numeric(sbar[1])
+  sbar_y <- as.numeric(sbar[2])
+  step_max <- (sqrt(5.99) * upsilon) + 3*upsilon
+  
+  # Subset statespace for local evaluation
+  local_ss <- landscape %>%
+    filter(x <= (sbar_x+step_max) & x >= (sbar_x-step_max)) %>%
+    filter(y <= (sbar_y+step_max) & y >= (sbar_y-step_max)) %>%
+    mutate(cell_true = cellFromXY(landscape_r, xy = local_ss)) %>%
+    mutate(cell = 1:n())
+  
+  local_ss_r <- rasterFromXYZ(local_ss[,1:3])
+  
+  # Check this out
+  if(TRUE){
+    plot(local_ss_r)
+    points(sbar)
+    draw.circle(x = sbar_x, y = sbar_y, radius = sqrt(5.99)*upsilon)
+  }
+  
+  # Create vector of steps
+  s.grid <- rep(NA, nfix)
+  s.grid[1] <- extract(x = local_ss_r, y = sbar, cellnumber=T)[1]
+  
+  
+  # Loop to generate fix locations
+  for(i in 2:nfix){
+    
+    # If the animal doesn't move, assign same loc and skip the rest
+    if(moved[i]==0){
+      s.grid[i] <- s.grid[i-1]
+      next # Next mean skip the rest of the for loop
+    }
+    
+    # If the animal moved...
+    
+    # Calculate ecological distance from the last position to each pixel (for upsilon)
+    D <- costDistance(
+      tr1Corr,  
+      local_ss %>% filter(cell == s.grid[i-1]) %>% select(x,y) %>% as.matrix(),
+      local_ss %>% select(x,y) %>% as.matrix())
+    
+    # Calculate euclidean distance from the activity center to each pixel (for sigma)
+    Dac <- e2dist(
+      x = local_ss %>% filter(cell == s.grid[i-1]) %>% select(x,y) %>% as.matrix(),
+      y = local_ss %>% select(x,y) %>% as.matrix()
+    )
+    
+    # Create a movement kernel using ecoD & upsilon and eucDac & sigma
+    kern <- exp((-D*D/(2*upsilon*upsilon))  - (Dac*Dac/(2*sigma*sigma)) )
+    
+    # Assign the current position a probability of zero (can't stay in the same spot, b/c Psi=1)
+    kern[s.grid[i-1]] <- 0
+    
+    # Normalize the kernel cell values into probabilities
+    kern <- kern/rowSums(kern)
+    
+    # Plot all of this
+    if(FALSE){
+      par(mfrow=c(2,2))
+      
+      # Landscape
+      plot(local_ss_r)
+      
+      # Eco dist
+      plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(D))))
+      
+      # Straight dist
+      plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(Dac))))
+      
+      # Kern
+      plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(kern))))
+      
+      par(mfrow=c(1,1))
+    }
+    
+    # Sample a pixel for the next step using kernel
+    s.grid[i]<- sample( 1:length(kern), 1, prob=kern)
+  }
+  
+  #----Organize track----
+
+  # Compile track from state-space using sampled cellnumbers
+  obs <- local_ss[s.grid,c("x","y")]
+  obs <- as.data.frame(obs)
+  rownames(obs) <- NULL
+  
+  # RETURN RESULT
+  tracks[[j]] <- cbind(obs, id = j)
+  
+}
+
+# Post-cluster cleanup
+stopCluster(cl)
+tf <- Sys.time()
+tf-t0
+
+
+# Combine tracks into a df
+telemetered_df = do.call(rbind, tracks) %>%
+  as.data.frame() %>%
+  group_by(id) %>%
+  mutate(times = 1:n()) %>%
+  ungroup()
+
   
 
 
