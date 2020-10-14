cb2db233a03ae08295a72b3321479ce2cdf95c45
Gaetan Dupont <gld44@cornell.edu>
2020-10-14 19:12:08 -0400

    First run of  10 sims for traps

--- R/gates_new/nybears_setup.R
+++ R/gates_new/nybears_setup.R
@@ -79,7 +79,7 @@ p1 <- ggplot() +
   coord_equal() +
   theme_minimal()
 
-p1text1 <- paste0(
+text1 <- paste0(
   "N = ", N, "\n",
   "Psi = ", psi, "\n",
   "Upsilon = ", upsilon, "\n",
@@ -119,136 +119,166 @@ tr1Corr <-geoCorrection(
 
 
 
-"SIMULATE MOVEMENT"
 
-# Parallel setup
-ncores = detectCores()-1 # Number of available cores -1 to leave for computer
-cl = makeCluster(ncores) # Make the cluster with that many cores
-registerDoParallel(cl)  
-clusterExport(cl, varlist = c("e2dist"), envir = environment()) # Export required function to the cores
 
-# Data-collection list
-tracks = list()
+tracks_all <- list()
 
-# Cluster!
-t0 <- Sys.time()
-tracks <- foreach(j=1:1, .packages = c(.packages())) %dopar% {
+
+for(sim in 1:10){
   
-  # Generating movement based on pr(move)
-  moved <- rbinom(nfix, 1, psi) # This should be different for each individual
+  set.seed(sim)
   
-  # Get starting position for the individual
-  sbar <- acs[j,]
-  sbar_x <- as.numeric(sbar[1])
-  sbar_y <- as.numeric(sbar[2])
-  step_max <- (sqrt(5.99) * sigma) + 3*upsilon
+  "SIMULATE MOVEMENT"
   
-  # Subset statespace for local evaluation
-  local_ss <- landscape %>%
-    filter(x <= (sbar_x+step_max) & x >= (sbar_x-step_max)) %>%
-    filter(y <= (sbar_y+step_max) & y >= (sbar_y-step_max)) %>%
-    mutate(cell_true = cellFromXY(landscape_r, xy = .)) %>%
-    mutate(cell = 1:n())
+  # Parallel setup
+  ncores = detectCores()-1 # Number of available cores -1 to leave for computer
+  cl = makeCluster(ncores) # Make the cluster with that many cores
+  registerDoParallel(cl)  
+  clusterExport(cl, varlist = c("e2dist"), envir = environment()) # Export required function to the cores
   
-  # Make local ss into raster
-  local_ss_r <- rasterFromXYZ(local_ss[,1:3])
+  # Data-collection list
+  tracks = list()
   
-  # Check this out
-  if(F){
-    plot(local_ss_r)
-    points(sbar)
-    draw.circle(x = sbar_x, y = sbar_y, radius = sqrt(5.99)*sigma)
-  }
-  
-  # Create vector of steps
-  s.grid <- rep(NA, nfix)
-  s.grid[1] <- extract(x = local_ss_r, y = sbar, cellnumber=T)[1]
-  
-  # Calculate ecological distance from the activity center to each pixel (for sigma)
-  # This only has to happen oncemoved
-  Dac <- costDistance(
-    tr1Corr,  
-    c(sbar_x, sbar_y),
-    local_ss %>% select(x,y) %>% as.matrix())
-  
-  
-  # Loop to generate fix locations
-  for(i in 2:nfix){
+  # Cluster!
+  t0 <- Sys.time()
+  tracks <- foreach(j=1:N, .packages = c(.packages())) %dopar% {
     
-    # If the animal doesn't move, assign same loc and skip the rest
-    if(moved[i]==0){
-      s.grid[i] <- s.grid[i-1]
-      next # Next mean skip the rest of the for loop
+    # Generating movement based on pr(move)
+    moved <- rbinom(nfix, 1, psi) # This should be different for each individual
+    
+    # Get starting position for the individual
+    sbar <- acs[j,]
+    sbar_x <- as.numeric(sbar[1])
+    sbar_y <- as.numeric(sbar[2])
+    step_max <- (sqrt(5.99) * sigma) + 3*upsilon
+    
+    # Subset statespace for local evaluation
+    local_ss <- landscape %>%
+      filter(x <= (sbar_x+step_max) & x >= (sbar_x-step_max)) %>%
+      filter(y <= (sbar_y+step_max) & y >= (sbar_y-step_max)) %>%
+      mutate(cell_true = cellFromXY(landscape_r, xy = .)) %>%
+      mutate(cell = 1:n())
+    
+    # Make local ss into raster
+    local_ss_r <- rasterFromXYZ(local_ss[,1:3])
+    
+    # Check this out
+    if(F){
+      plot(local_ss_r)
+      points(sbar)
+      draw.circle(x = sbar_x, y = sbar_y, radius = sqrt(5.99)*sigma)
     }
     
-    ## If the animal moved...
+    # Create vector of steps
+    s.grid <- rep(NA, nfix)
+    s.grid[1] <- extract(x = local_ss_r, y = sbar, cellnumber=T)[1]
     
-    # Calculate ecological distance from the last position to each pixel (for upsilon)
-    D <- costDistance(
+    # Calculate ecological distance from the activity center to each pixel (for sigma)
+    # This only has to happen oncemoved
+    Dac <- costDistance(
       tr1Corr,  
-      local_ss %>% filter(cell == s.grid[i-1]) %>% select(x,y) %>% as.matrix(),
+      c(sbar_x, sbar_y),
       local_ss %>% select(x,y) %>% as.matrix())
     
     
-    # Distance from activity center only needs to be calculated once (above)
-    
-    # Create a movement kernel using ecoD & upsilon and eucDac & sigma
-    kern <- exp((-D*D/(2*upsilon*upsilon))  - (Dac*Dac/(2*sigma*sigma)) )
-    
-    # Assign the current position a probability of zero (can't stay in the same spot, b/c Psi=1)
-    kern[s.grid[i-1]] <- 0
-    
-    # Normalize the kernel cell values into probabilities
-    kern <- kern/rowSums(kern)
-    
-    # Plot all of this
-    if(F){
-      par(mfrow=c(2,2))
+    # Loop to generate fix locations
+    for(i in 2:nfix){
       
-      # Landscape
-      plot(local_ss_r, main = "Covariate")
+      # If the animal doesn't move, assign same loc and skip the rest
+      if(moved[i]==0){
+        s.grid[i] <- s.grid[i-1]
+        next # Next mean skip the rest of the for loop
+      }
       
-      # Step dist
-      plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(D))), main = "Upsilon eco")
+      ## If the animal moved...
       
-      # AC dist
-      plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(Dac))), main = "Sigma eco")
+      # Calculate ecological distance from the last position to each pixel (for upsilon)
+      D <- costDistance(
+        tr1Corr,  
+        local_ss %>% filter(cell == s.grid[i-1]) %>% select(x,y) %>% as.matrix(),
+        local_ss %>% select(x,y) %>% as.matrix())
       
-      # Kern
-      plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(kern))), main = "Next step probabilities")
-      lines(local_ss[s.grid,c("x","y")])
       
-      par(mfrow=c(1,1))
+      # Distance from activity center only needs to be calculated once (above)
+      
+      # Create a movement kernel using ecoD & upsilon and eucDac & sigma
+      kern <- exp((-D*D/(2*upsilon*upsilon))  - (Dac*Dac/(2*sigma*sigma)) )
+      
+      # Assign the current position a probability of zero (can't stay in the same spot, b/c Psi=1)
+      kern[s.grid[i-1]] <- 0
+      
+      # Normalize the kernel cell values into probabilities
+      kern <- kern/rowSums(kern)
+      
+      # Plot all of this
+      if(F){
+        par(mfrow=c(2,2))
+        
+        # Landscape
+        plot(local_ss_r, main = "Covariate")
+        
+        # Step dist
+        plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(D))), main = "Upsilon eco")
+        
+        # AC dist
+        plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(Dac))), main = "Sigma eco")
+        
+        # Kern
+        plot(rasterFromXYZ(cbind(local_ss[,1:2], z = as.numeric(kern))), main = "Next step probabilities")
+        lines(local_ss[s.grid,c("x","y")])
+        
+        par(mfrow=c(1,1))
+      }
+      
+      # Sample a pixel for the next step using kernel
+      s.grid[i]<- sample( 1:length(kern), 1, prob=kern)
     }
     
-    # Sample a pixel for the next step using kernel
-    s.grid[i]<- sample( 1:length(kern), 1, prob=kern)
+    #----Organize track----
+    
+    # Compile track from state-space using sampled cellnumbers
+    obs <- local_ss[s.grid,c("x","y")]
+    obs <- as.data.frame(obs)
+    rownames(obs) <- NULL
+    
+    # RETURN RESULT
+    tracks[[j]] <- cbind(obs, id = j)
+    
   }
   
-  #----Organize track----
-
-  # Compile track from state-space using sampled cellnumbers
-  obs <- local_ss[s.grid,c("x","y")]
-  obs <- as.data.frame(obs)
-  rownames(obs) <- NULL
+  # Post-cluster cleanup
+  stopCluster(cl)
+  tf <- Sys.time()
+  tf-t0
   
-  # RETURN RESULT
-  tracks[[j]] <- cbind(obs, id = j)
+  
+  # Combine tracks into a df
+  telemetered_df = do.call(rbind, tracks) %>%
+    as.data.frame() %>%
+    group_by(id) %>%
+    mutate(times = 1:n()) %>%
+    ungroup()
+  
+  tracks_all[[sim]] <- telemetered_df
+  
+  
   
 }
 
-# Post-cluster cleanup
-stopCluster(cl)
-tf <- Sys.time()
-tf-t0
 
 
-# Combine tracks into a df
-telemetered_df = do.call(rbind, tracks) %>%
-  as.data.frame() %>%
-  group_by(id) %>%
-  mutate(times = 1:n()) %>%
-  ungroup()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Plot of state-space, surface, & track
